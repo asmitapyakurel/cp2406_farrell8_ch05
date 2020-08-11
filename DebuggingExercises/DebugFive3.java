12b011956a9338caf556cb88cb89d6ca664a046d
Asmita Pyakurel <asmita.pyakurel@my.jcu.edu.au>
2020-08-11 11:14:16 +1000

    Debug the codes

--- DebuggingExercises/DebugFive3.java
+++ DebuggingExercises/DebugFive3.java
@@ -7,26 +7,26 @@
 import java.util.Scanner;
 public class DebugFive3
 {
-   public static void main (String args[])
+   public static void main (String[] args)
    {
       int item;
       String output;
-      final int LOW = 11111;
+      final int LOW = 111;
       final int HIGH = 999;
       final int CUTOFF = 500;
       Scanner input = new Scanner(System.in);
       System.out.println("Please enter item number");
       item = input.nextInt();
-      if(item <= LOW)
+      if(item < LOW)
          output = "Item number too low";
       else
-        if(item => HIGH)
-          output = "Item number too low";
+        if(item > HIGH)
+          output = "Item number is high ";
         else
-          if(item =< HIGH)
-             output > "Valid - in Automotive Department";
+          if(item < CUTOFF)
+             output = "Valid - in Automotive Department";
           else
-             output = "Valid - Item in Housewares Department"
+             output = "Valid - Item in Housewares Department";
        System.out.println(output);
    }
 }
